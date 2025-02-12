90cd7668a21de863b30286ff926ccfe6b44b2525
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2025-02-12 17:50:14 +0100

    Add sound support and org clock sound for macOS

--- mac/3-modes.el
+++ mac/3-modes.el
@@ -22,3 +22,39 @@
 
 ;; SQL
 ;; to be defined (if needed)
+
+
+
+
+;; Add sound support
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; For MacOS, fix "This Emacs binary lacks sound support"
+;; 
+;; source info:
+;; - https://github.com/leoliu/play-sound-osx/blob/master/play-sound.el
+;; - update according to https://github.com/leoliu/play-sound-osx/issues/2#issuecomment-1088360638
+;; - https://www.charlbotha.com/til/fix-emacs-sound-on-macos
+(when (eq system-type 'darwin)
+  (unless (and (fboundp 'play-sound-internal)
+               (subrp (symbol-function 'play-sound-internal)))
+    (defun play-sound-internal (sound)
+      "Internal function for `play-sound' (which see)."
+      (or (eq (car-safe sound) 'sound)
+          (signal 'wrong-type-argument (list sound)))
+ 
+      (cl-destructuring-bind (&key file data volume device)
+          (cdr sound)
+ 
+        (and (or data device)
+             (error "DATA and DEVICE arg not supported"))
+ 
+        (apply #'start-process "afplay" nil
+               "afplay" (append (and volume (list "-v" volume))
+                                (list (expand-file-name file data-directory))))))))
+
+;; ORG mode - add default clock sound
+(setq org-clock-sound "~/Downloads/ding.wav")
+
+
+
